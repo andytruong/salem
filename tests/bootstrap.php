b42a5112fd513e6430a902cf6fe0400ee0f4317b
Andy Truong <thehongtt@gmail.com>
2014-08-15 16:51:03 +0700

    Add missing tests's bootstrap.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+$locations[] = __DIR__ . "/../vendor/autoload.php";
+$locations[] = __DIR__ . "/../../../autoload.php";
+
+foreach ($locations as $location) {
+    if (is_file($location)) {
+        $loader = require $location;
+        $loader->addPsr4('AndyTruong\\Salem\\TestCases\\', __DIR__ . '/salem');
+        $loader->addPsr4('AndyTruong\\Salem\\Fixtures\\', __DIR__ . '/fixtures');
+        break;
+    }
+}
